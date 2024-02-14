d61be36ce9ecd9634b84e8033cf72a9ba87a9124
Shad Sajid <88870507+s-sajid@users.noreply.github.com>
2024-02-13 19:02:45 -0700

    Add Equals tests

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -6,22 +6,58 @@ import org.jfree.data.Range;
 import org.junit.*;
 
 public class RangeTest {
-    private Range exampleRange;
+    private Range exampleRange1;
+    private Range exampleRange2;
+    private Range exampleRange3;
+    private Range exampleRange4;
+    
     @BeforeClass public static void setUpBeforeClass() throws Exception {
     }
 
 
     @Before
-    public void setUp() throws Exception { exampleRange = new Range(-1, 1);
+    public void setUp() throws Exception { 
+    	exampleRange1 = new Range(-1, 1);
+    	exampleRange2 = new Range(-2, 2);
+    	exampleRange3 = new Range(-1, 1);
+    	exampleRange4 = exampleRange3;
     }
-
+    
+    @Test
+    public void testEqualsNull() {
+        assertFalse(exampleRange1.equals(null));
+    }
+    
+    @Test
+    public void testEqualsDifferentRanges() {
+        assertFalse(exampleRange1.equals(exampleRange2));
+    }
 
     @Test
-    public void centralValueShouldBeZero() {
-        assertEquals("The central value of -1 and 1 should be 0",
-        0, exampleRange.getCentralValue(), .000000001d);
+    public void testEqualsReflexive() {
+        assertTrue(exampleRange1.equals(exampleRange1));
+    }
+    
+    @Test
+    public void testEqualsSymmetric() {
+        assertTrue(exampleRange1.equals(exampleRange3));
+        assertTrue(exampleRange3.equals(exampleRange1));
+    }
+    
+    @Test
+    public void testEqualsTransitive() {
+        assertTrue(exampleRange1.equals(exampleRange3));
+        assertTrue(exampleRange3.equals(exampleRange4));
+        assertTrue(exampleRange1.equals(exampleRange4));
     }
 
+    
+    @Test
+    public void testEqualsConsistency() {
+        assertFalse(exampleRange1.equals(null));
+    }
+
+    
     @After
     public void tearDown() throws Exception {
     }
