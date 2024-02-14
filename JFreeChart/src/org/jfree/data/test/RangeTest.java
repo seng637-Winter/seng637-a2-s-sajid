f5afc66c8ee6c8d85a961dce23a618aa46ddc375
Shad Sajid <88870507+s-sajid@users.noreply.github.com>
2024-02-13 19:17:27 -0700

    Refactor testing methods

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -6,59 +6,56 @@ import org.jfree.data.Range;
 import org.junit.*;
 
 public class RangeTest {
-    private Range exampleRange1;
-    private Range exampleRange2;
-    private Range exampleRange3;
-    private Range exampleRange4;
+    private Range r1;
+    private Range r2;
+    private Range r3;
+    private Range r4;
     
     @BeforeClass public static void setUpBeforeClass() throws Exception {
     }
 
     @Before
     public void setUp() throws Exception { 
-    	exampleRange1 = new Range(-1, 1);
-    	exampleRange2 = new Range(-2, 2);
-    	exampleRange3 = new Range(-1, 1);
-    	exampleRange4 = exampleRange3;
+    	r1 = new Range(-1, 1);
+    	r2 = new Range(-2, 2);
+    	r3 = new Range(-1, 1);
+    	r4 = r3;
     }
     
     @Test
     public void testEqualsNull() {
-        assertFalse(exampleRange1.equals(null));
+        assertFalse(r1.equals(null));
     }
     
     @Test
     public void testEqualsDifferentRanges() {
-        assertFalse(exampleRange1.equals(exampleRange2));
+        assertFalse(r1.equals(r2));
     }
 
     @Test
     public void testEqualsReflexive() {
-        assertTrue(exampleRange1.equals(exampleRange1));
+        assertTrue(r1.equals(r1));
     }
     
     @Test
     public void testEqualsSymmetric() {
-        assertTrue(exampleRange1.equals(exampleRange3));
-        assertTrue(exampleRange3.equals(exampleRange1));
+    	assertTrue(r1.equals(r3) && r3.equals(r1));
     }
     
     @Test
     public void testEqualsTransitive() {
-        assertTrue(exampleRange1.equals(exampleRange3));
-        assertTrue(exampleRange3.equals(exampleRange4));
-        assertTrue(exampleRange1.equals(exampleRange4));
+    	assertTrue(r1.equals(r3) && r3.equals(r4) && r1.equals(r4));
     }
 
     @Test
     public void testEqualsConsistency() {
-        assertFalse(exampleRange1.equals(null));
+        assertTrue(r1.equals(r3) == r3.equals(r1));
     }
     
     @Test
     public void testEqualsNegative() {
-    	Range exampleRange5 = new Range(1, -1);
-        assertFalse(exampleRange1.equals(exampleRange5));
+    	Range r5 = new Range(1, -1);
+        assertFalse(r1.equals(r5));
     }
 
     @After
