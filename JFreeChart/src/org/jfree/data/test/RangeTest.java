d800f45a451c9c04932dd12b17e6cf7479338878
Shad Sajid <88870507+s-sajid@users.noreply.github.com>
2024-02-13 20:12:25 -0700

    Add comments and update BVT range

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -15,37 +15,43 @@ public class RangeTest {
 
     @Before
     public void setUp() throws Exception { 
-    	r1 = new Range(-1, 1);
-    	r2 = new Range(-2, 2);
-    	r3 = new Range(-1, 1);
+    	r1 = new Range(-10, 10);
+    	r2 = new Range(-2, 7);
+    	r3 = new Range(-10, 10);
     }
     
+    // Test case covering null comparison where r1 != null
     @Test
     public void testEqualsNull() {
         assertFalse(r1.equals(null));
     }
     
+    // Test case covering different range comparison where r1 != r2
     @Test
     public void testEqualsDifferentRanges() {
         assertFalse(r1.equals(r2));
     }
 
+    // Test case covering reflexive comparison where r1 == r1
     @Test
     public void testEqualsReflexive() {
         assertTrue(r1.equals(r1));
     }
     
+    // Test case covering symmetric comparison where r1 == r3
     @Test
     public void testEqualsSymmetric() {
     	assertTrue(r1.equals(r3) && r3.equals(r1));
     }
     
+    // Test case covering transitive comparison where r1 == r3 == r4
     @Test
     public void testEqualsTransitive() {
     	Range r4 = r3;
     	assertTrue(r1.equals(r3) && r3.equals(r4) && r1.equals(r4));
     }
 
+    // Test case covering consistency of equals method where r1 == r3
     @Test
     public void testEqualsConsistency() {
         assertTrue(r1.equals(r3) == r3.equals(r1));
