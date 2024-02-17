7fb7f17b8c8b5a5710ccb144dc7ba1f1f8673b13
Shad Sajid <88870507+s-sajid@users.noreply.github.com>
2024-02-16 23:13:25 -0700

    Update variable names

--- JFreeChart/src/org/jfree/data/test/EqualsTest.java
+++ JFreeChart/src/org/jfree/data/test/EqualsTest.java
@@ -6,59 +6,55 @@ import org.jfree.data.Range;
 import org.junit.*;
 
 public class EqualsTest {
+    private Range testRange;
     
     @BeforeClass public static void setUpBeforeClass() throws Exception {
     }
 
     @Before
-    public void setUp() throws Exception { 
+    public void setUp() throws Exception {
+    	testRange = new Range(-10, 10);
     }
     
     // Test case covering null comparison where r1 != null
     @Test
     public void testEqualsNull() {
-    	Range r = new Range(-10, 10);
-        assertFalse(r.equals(null));
+        assertFalse(testRange.equals(null));
     }
     
     // Test case covering different range comparison where r1 != r2
     @Test
     public void testEqualsDifferentRanges() {
-    	Range r1 = new Range(-10, 10);
-    	Range r2 = new Range(-2, 7);
-        assertFalse(r1.equals(r2));
+    	Range differentRange = new Range(-2, 7);
+        assertFalse(testRange.equals(differentRange));
     }
 
     // Test case covering reflexive comparison where r == r
     @Test
     public void testEqualsReflexive() {
-    	Range r = new Range(-10, 10);
-        assertTrue(r.equals(r));
+        assertTrue(testRange.equals(testRange));
     }
     
     // Test case covering symmetric comparison where r1 == r2
     @Test
     public void testEqualsSymmetric() {
-    	Range r1 = new Range(-10, 10);
-    	Range r2 = new Range(-10, 10);
-    	assertTrue(r1.equals(r2) && r2.equals(r1));
+    	Range testRange2 = new Range(-10, 10);
+    	assertTrue(testRange.equals(testRange2) && testRange2.equals(testRange));
     }
     
     // Test case covering transitive comparison where r1 == r2 == r3
     @Test
     public void testEqualsTransitive() {
-    	Range r1 = new Range(-10, 10);
-    	Range r2 = new Range(-10, 10);
-    	Range r3 = new Range(-10, 10);
-    	assertTrue(r1.equals(r2) && r2.equals(r3) && r1.equals(r3));
+    	Range testRange2 = new Range(-10, 10);
+    	Range testRange3 = new Range(-10, 10);
+    	assertTrue(testRange.equals(testRange2) && testRange2.equals(testRange3) && testRange.equals(testRange3));
     }
 
     // Test case covering consistency of equals method where r1 == r2
     @Test
     public void testEqualsConsistency() {
-    	Range r1 = new Range(-10, 10);
-    	Range r2 = new Range(-10, 10);
-        assertTrue(r1.equals(r2) == r2.equals(r1));
+    	Range testRange2 = new Range(-10, 10);
+        assertTrue(testRange.equals(testRange2) == testRange2.equals(testRange));
     }
         
     @After
